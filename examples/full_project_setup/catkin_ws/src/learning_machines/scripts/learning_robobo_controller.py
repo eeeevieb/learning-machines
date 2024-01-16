9ea6f29dbf826b5535fd29cfc3b13d48a017d381
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2024-01-16 11:51:16 +0100

    fixed arguments in run script

--- examples/full_project_setup/catkin_ws/src/learning_machines/scripts/learning_robobo_controller.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/scripts/learning_robobo_controller.py
@@ -7,16 +7,16 @@ from learning_machines import run_task_0
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-h', '--hardware', action='store_true', default=False, help='whether to run IRL')
-    parser.add_argument('-s', '--simulator', action='store_true', default=False, help='whether to run in simulation')
+    parser.add_argument('-r', '--reallife', action='store_true', default=False, help='whether to run IRL')
+    parser.add_argument('-s', '--simulation', action='store_true', default=False, help='whether to run in simulation')
 
 
     args = parser.parse_args()
 
-    if args['hardware']:
+    if args.reallife:
         rob = HardwareRobobo(camera=False)
-    elif args['simulator']:
-        rob = SimulationRobobo(camera=False)
+    elif args.simulation:
+        rob = SimulationRobobo()
         pass
     else:
         raise ValueError('should provide a flag to run on robot or simulator')
