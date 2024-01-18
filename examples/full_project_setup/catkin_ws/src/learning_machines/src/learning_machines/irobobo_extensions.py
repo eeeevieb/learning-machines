1c93efac3644c9ad3bd1b722941575c89b30b824
kuhusinha9 <kuhusinha92@gmail.com>
2024-01-18 13:54:50 +0100

    updated reward function

--- examples/full_project_setup/catkin_ws/src/learning_machines/src/learning_machines/irobobo_extensions.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/src/learning_machines/irobobo_extensions.py
@@ -10,17 +10,21 @@ def get_number_of_target_pixels(img):
     px_num = blue.shape[0]*blue.shape[1]
     mask = (blue > green) & (blue > red)
     count = np.count_nonzero(mask)
-    return count / px_num
+    return (count / px_num) + 0.001
 
 
 def get_reward(rob:IRobobo):
     image = rob.get_image_front()
     pixels = get_number_of_target_pixels(image)
-    obs=max(rob.read_irs())
-    obstacles= obs
-    #orient = rob.read_wheels()
 
-    reward = pixels * (100-obstacles) # * (1-abs(orient.wheel_pos_l - orient.wheel_speed_r)) # check 
+    obstacles = (np.clip(max(rob.read_irs()), 0, 1000) / 1000) - 0.001
+
+    orient = rob.read_wheels()
+    ori = (abs(orient.wheel_pos_l - orient.wheel_pos_r) /7200)
+    
+    reward = pixels * (1-obstacles) * (2 - ori) # check 
+    print(f"pixels: {pixels}, obs: {obstacles}, orient: {ori}, reward: {reward}")
+    print(orient)
     return reward
 
 
@@ -42,9 +46,9 @@ def do_action(rob:IRobobo, action):
     if action == 'move_forward':
         block = rob.move(50, 50, 200)
     elif action == 'turn_right':
-        block = rob.move(50, 0, 50)
+        block = rob.move(50, -50, 50)
     elif action == 'turn_left':
-        block = rob.move(0, 50, 50)
+        block = rob.move(-50, 50, 50)
     else:
         block = rob.move(-20, -20, 200)
     return block
